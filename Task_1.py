43cfc794cdf22cc737668f55799addc9152ead65
Victoria Bakarikova-Kirichenko <victoriabakarikova2@gmail.com>
2025-03-19 12:32:50 +0300

    Add Task 1 done

--- Task_1.py
+++ Task_1.py
@@ -1,4 +1,4 @@
 first_name=input("Введите Имя: ")
 last_name=input("Введите фамилию: ")
 father_name=input("Введите отчество: ")
-print("ФИО: ", last_name.capitalize() +" " + first_name[0].upper() + ". " + father_name[0].upper() +".")
+print("Ваше ФИО: ", last_name.capitalize() +" " + first_name[0].upper() + ". " + father_name[0].upper() +".")
