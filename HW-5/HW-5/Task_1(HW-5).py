d94883238e9da44bba5fbfad345d277f662f7b56
Victoria Bakarikova-Kirichenko <victoriabakarikova2@gmail.com>
2025-04-17 11:26:50 +0300

    Add task1 hw-5

---
+++ HW-5/HW-5/Task_1(HW-5).py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self._top_node = None
+        self._size = 0
+
+    def push(self, item):
+        new_node = Node(item)
+        new_node.next = self._top_node
+        self._top_node = new_node
+        self._size += 1
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError('Stack is empty')
+        removed_item = self._top_node.data
+        self._top_node = self._top_node.next
+        self._size -= 1
+        return removed_item
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError('Stack is empty')
+        return self._top_node.data
+
+    def is_empty(self):
+        return self._top_node is None
+
+    def size(self):
+        return self._size
+
+    def display(self):
+        current_node = self._top_node
+        current_nodes = []
+        while current_node:
+            current_nodes.append(current_node.data)
+            current_node = current_node.next
+        print("Stack:", ", ".join(map(str, current_nodes)))
+
+
+stack = Stack()
+stack.push(23)
+stack.push(5)
+stack.push(76)
+stack.push(24)
+stack.push(34)
+
+stack.display()
+print(stack.pop())
+print(stack.pop())
+print(stack.peek())
